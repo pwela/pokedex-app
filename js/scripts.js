4e376e6752aab44907bd7b13acc5839cac10e0a8
pwela <139175113+pwela@users.noreply.github.com>
2023-07-26 23:27:03 -0600

    Pokemon Creation
    Old code in scripts.js
    Created 3 pokemons

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
-alert('Hello world');
-let favoriteFood = 'Ceviche';
-document.write(favoriteFood);
+// creating an empty array list of pokemoon
+let pokemonList=[];
+
+// Creating 3 pokemons
+pokemonList[0]={name:'Bulbasaur', height:7, types:['grass', 'poison']};
+pokemonList[1]={name:'Pikachu', height:4, types:['electric']};
+pokemonList[2]={name:'BButterfree', height:11, types:['bug', 'flying']};
