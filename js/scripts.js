e1f9e51471a0a965a95786e43b3a4da9debf627c
pwela <139175113+pwela@users.noreply.github.com>
2023-07-29 02:17:36 -0600

    Task Exercise 1.5
    Performed all the changes related to exercise 1.5

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,65 @@
-// Exercise 1.2
-// creating an empty array list of pokemoon and initializing with 3 pokemons
+// Creating IIFE
+let pokemonRepository = (function() {
+    let pokemonList = [
+      {
+        name: 'Bulbasaur',
+        height: .7,
+        types: ['grass', 'poison']
+      },
+      {
+        name: 'Pikachu',
+        height: .4,
+        types: ['electric']
+      },
+      {
+        name: 'Butterfree',
+        height: 1.1,
+        types: ['bug', 'flying']
+      }
+    ];
+  
+    //function getAll 
+    function getAll() {
+      return pokemonList;
+    }
+    // function add
+    function add(pokemon) {
+      pokemonList.push(pokemon);
+    }
+  
+    return {
+      add: add,
+      getAll: getAll,
+  
+    };
+  
+  })();
+  
+  // display pokemon name and height
+  
+  pokemonRepository.getAll().forEach( function (pokemon){
+  document.write("Pokemon name: " + pokemon.name + ", height:" + pokemon.height + "m. ");
+      
+        // check pokemon height
+        if (pokemon.height === 1.1) {
+          document.write("Wow that's big!" + "<br>");
+        }
+        else {
+          document.write("<br>");
+        }
+    
+  });
 
-let pokemonList=[
-    {
-        name:'Bulbasaur',
-        height:.7,
-        types:['grass', 'poison']
-    },
-    {
-        name:'Pikachu',
-        height:.4,
-        types:['electric']},
-    {
-        name:'Butterfree',
-        height:1.1, 
-        types:['bug', 'flying']
-    }
-];
-
-// Exercise 1.3
-// display pokemon name and height
-for(i=0; i<pokemonList.length; i++) {
-    document.write(i+1 + ". Pokemon name: " + pokemonList[i].name + ", height:" + pokemonList[i].height + "m. ");
-   
-    // check pokemon height
-    if(pokemonList[i].height===1.1) {
-        document.write("Wow that's big!" + "<br>");
-    }
-    else {
-        document.write("<br>");
-    }
-}
+// Part 1
+// pokemonList.forEach(function (pokemon) {
+//     document.write("Pokemon name: " + pokemon.name + ", height:" + pokemon.height + "m. ");
+    
+//       // check pokemon height
+//       if (pokemon.height === 1.1) {
+//         document.write("Wow that's big!" + "<br>");
+//       }
+//       else {
+//         document.write("<br>");
+//       }
+    
+//     });
