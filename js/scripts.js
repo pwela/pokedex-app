ebfc5c1f4c3aff486984c5f77da1eb12d12f42fa
pwela <139175113+pwela@users.noreply.github.com>
2023-07-30 17:04:05 -0600

    Tasks with Bonus
    Added bonus tasks for exercise 1.5

--- js/scripts.js
+++ js/scripts.js
@@ -24,7 +24,12 @@ let pokemonRepository = (function() {
     }
     // function add
     function add(pokemon) {
-      pokemonList.push(pokemon);
+      // the second part of the consition in not norking : && Object.keys(pokemon).toString === [name,height,types].toString()
+      if (typeof pokemon == "object" ) {
+        pokemonList.push(pokemon);
+
+      }
+     else window.alert("Incorrect data type");
     }
   
     return {
@@ -35,31 +40,50 @@ let pokemonRepository = (function() {
   
   })();
   
-  // display pokemon name and height
-  
-  pokemonRepository.getAll().forEach( function (pokemon){
-  document.write("Pokemon name: " + pokemon.name + ", height:" + pokemon.height + "m. ");
-      
-        // check pokemon height
-        if (pokemon.height === 1.1) {
-          document.write("Wow that's big!" + "<br>");
-        }
-        else {
-          document.write("<br>");
-        }
-    
-  });
+  // function to display pokemon
 
-// Part 1
-// pokemonList.forEach(function (pokemon) {
-//     document.write("Pokemon name: " + pokemon.name + ", height:" + pokemon.height + "m. ");
-    
-//       // check pokemon height
-//       if (pokemon.height === 1.1) {
-//         document.write("Wow that's big!" + "<br>");
-//       }
-//       else {
-//         document.write("<br>");
-//       }
-    
-//     });
+  function displayPokemon (pokemon){
+    document.write("Pokemon name: " + pokemon.name + ", height:" + pokemon.height + "m. ");
+        
+          // check pokemon height
+          if (pokemon.height > 1) {
+            document.write("Wow that's big!" + "<br>");
+          }
+          else {
+            document.write("<br>");
+          }
+      
+    }
+  
+  // filter all pokemons with height < 1
+ function filterPokemon (pokemon){
+  if (pokemon.height <1) {
+  return pokemon;
+  }
+}
+
+function addingParagraph (string) {
+  document.write("<p>" + string + "<p>");  
+}
+
+// display pokemon name and height
+  pokemonRepository.getAll().forEach(displayPokemon);
+
+// Adding a paragraph
+ addingParagraph("Pokemons less than 1m");
+
+ // filtering and diplaying
+ let filteredPokemonRepository = pokemonRepository.getAll().filter(filterPokemon);
+ filteredPokemonRepository.forEach( displayPokemon);
+
+ addingParagraph("Adding a pokemon");
+pokemonRepository.add({name: 'Blastoise', height: 1.6, types: ['water']} );
+
+addingParagraph("display all pokemons");
+
+pokemonRepository.getAll().forEach(displayPokemon);
+
+addingParagraph("Pokemons less than 1m");
+ // filtering and diplaying
+filteredPokemonRepository = pokemonRepository.getAll().filter(filterPokemon);
+filteredPokemonRepository.forEach( displayPokemon);
