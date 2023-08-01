40e2061e35f67e392e1756ac528f59a1945dc880
pwela <139175113+pwela@users.noreply.github.com>
2023-08-01 00:40:37 -0600

    Exercise 1.7
    Performed tasks related to exercise 1.7

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,19 @@
 // Creating IIFE
 let pokemonRepository = (function() {
-    let pokemonList = [
-      {
-        name: 'Bulbasaur',
-        height: .7,
-        types: ['grass', 'poison']
-      },
-      {
-        name: 'Pikachu',
-        height: .4,
-        types: ['electric']
-      },
-      {
-        name: 'Butterfree',
-        height: 1.1,
-        types: ['bug', 'flying']
-      }
-    ];
-  
+    let pokemonList = [];
+    let apiUrl ="https://pokeapi.co/api/v2/pokemon/?limit=150";
+    let loadingMessage = document.querySelector('.loading-message');
+   
+ // Hide loading message
+ function hideLoadingMessage() {
+  loadingMessage.classList.add('displayMessage');
+ }   
+
+  // Show loading message
+  function showLoadingMessage() {
+    loadingMessage.classList.remove('displayMessage');
+   }   
+
 //function getAll the pokemons 
     function getAll() {
       return pokemonList;
@@ -51,7 +47,9 @@ let pokemonRepository = (function() {
 
    // Show pokemon details
     function showDetails(pokemon) {
-    console.log(pokemon.name);
+      loadDetails(pokemon).then(function () {
+        console.log(pokemon);
+      });
     }
    
   // This function adds a 'click' eventListener to pokemonButton and calls the methos to show pokemon details
@@ -61,21 +59,56 @@ let pokemonRepository = (function() {
       });
     }
 
+    function loadList() {
+      showLoadingMessage();
+      return fetch(apiUrl).then(function (response) {
+        hideLoadingMessage();
+        return response.json();
+      }).then(function (json) {
+        hideLoadingMessage();
+        json.results.forEach(function (item) {
+          let pokemon = {
+            name: item.name,
+            detailsUrl: item.url
+          };
+          add(pokemon);
+        });
+      }).catch(function (e) {
+        console.error(e);
+      })
+    }
+
+    function loadDetails(item) {
+      showLoadingMessage();
+      let url = item.detailsUrl;
+      return fetch(url).then(function (response) {
+        hideLoadingMessage();
+        return response.json();
+      }).then(function (details) {
+        hideLoadingMessage();
+        // Now we add the details to the item
+        item.imageUrl = details.sprites.front_default;
+        item.height = details.height;
+        item.types = details.types;
+      }).catch(function (e) {
+        console.error(e);
+      });
+    }
       return {
         add: add,
         getAll: getAll,
-        addListItem: addListItem
+        addListItem: addListItem,
+        loadList: loadList,
+        loadDetails : loadDetails
     
       };
 
   })();
  
-//Adding a pokemon
-pokemonRepository.add({name: 'Nidoking', height: 1.4, types: ['ground','poison']})
-
-//display all the pokemons
+pokemonRepository.loadList().then(function(){
+// add and display all the pokemons
 pokemonRepository.getAll().forEach ( function (pokemon){
-  pokemonRepository.addListItem(pokemon);
-
+pokemonRepository.addListItem(pokemon);
+});
 });
 
